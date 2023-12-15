90daaeff0cd5bd04030f7ea0cd2301a0cbf3143e
KangJin Kim <korea19800@gmail.com>
2023-12-14 23:18:29 -0700

    Delete startupF directory

--- startupF/public/login.js
+++
@@ -1,8 +0,0 @@
-// js for login -> index.html
-function login() {
-    const nameEl = document.querySelector("#name");
-    localStorage.setItem("userName", nameEl.value);
-    // move to posts page
-    window.location.href = "posts.html";
-  }
-  
